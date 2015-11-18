00a8d7bb764acb5912400b31110e4cc60b43ad4d
Kacper Kaśków <kacper.kaskow@apilia.pl>
2015-11-18 23:27:38 +0100

    added random initial settings

--- app/src/main/java/pl/jatustygne/fragment/TemperatureFragment.java
+++ app/src/main/java/pl/jatustygne/fragment/TemperatureFragment.java
@@ -1,5 +1,6 @@
 package pl.jatustygne.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,6 +9,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.yahoo.mobile.client.android.util.rangeseekbar.RangeSeekBar;
+
+import java.util.Random;
 
 /**
  * Created by Dawid Drozd aka Gelldur on 11/18/15.
@@ -19,6 +22,12 @@ public class TemperatureFragment extends Fragment {
 							 final Bundle savedInstanceState) {
 		final RangeSeekBar<Float> floatRangeSeekBar = new RangeSeekBar<>(getActivity());
 		floatRangeSeekBar.setRangeValues(30.F, 100.F);
+		floatRangeSeekBar.setColorFilter(Color.BLACK);
+		Random random = new Random();
+		float rand1 = random.nextFloat() % 100 + 30;
+		float rand2 = random.nextFloat() % 100 + 30;
+		floatRangeSeekBar.setSelectedMinValue(rand1 < rand2 ? rand1 : rand2);
+		floatRangeSeekBar.setSelectedMaxValue(rand1 < rand2 ? rand2 : rand1);
 //		floatRangeSeekBar.setOnRangeSeekBarChangeListener(new RangeSeekBar.OnRangeSeekBarChangeListener<Float>() {
 //			@Override
 //			public void onRangeSeekBarValuesChanged(RangeSeekBar<?> bar, Float minValue, Float maxValue) {
