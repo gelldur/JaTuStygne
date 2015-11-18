b8bf9b1ca07f3c858bdedf0fec6553dbfbefcb5a
Dawid Drozd <drozddawid.uam@gmail.com>
2015-11-18 15:27:38 +0100

    Integrating sensoro SDK + sample code
    It works!

--- app/src/main/java/pl/jatustygne/MainActivity.java
+++ app/src/main/java/pl/jatustygne/MainActivity.java
@@ -1,7 +1,13 @@
 package pl.jatustygne;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.sensoro.beacon.kit.Beacon;
+import com.sensoro.beacon.kit.BeaconManagerListener;
+import com.sensoro.beacon.kit.SensoroBeaconManager;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,41 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		SensoroBeaconManager sensoroBeaconManager = SensoroBeaconManager.getInstance(getApplicationContext());
+		if (sensoroBeaconManager.isBluetoothEnabled()) {
+			try {
+				sensoroBeaconManager.startService();
+			} catch (Exception e) {
+				e.printStackTrace(); // Fetch abnormal info
+			}
+		}
+
+		BeaconManagerListener beaconManagerListener = new BeaconManagerListener() {
+
+			@Override
+			public void onUpdateBeacon(ArrayList<Beacon> beacons) {
+				System.out.println("~~~~~~~~ I have beacons: " + beacons.size());
+				for (Beacon beacon : beacons) {
+					System.out.println(beacon.toString());
+				}
+				System.out.println("");
+			}
+
+			@Override
+			public void onNewBeacon(Beacon beacon) {
+				System.out.println("~~~~~~~~ New beacon");
+				System.out.println(beacon.toString());
+				System.out.println("");
+			}
+
+			@Override
+			public void onGoneBeacon(Beacon beacon) {
+				System.out.println("~~~~~~~~ Beacon gone!");
+				System.out.println(beacon.toString());
+				System.out.println("");
+			}
+		};
+		sensoroBeaconManager.setBeaconManagerListener(beaconManagerListener);
 	}
 }
