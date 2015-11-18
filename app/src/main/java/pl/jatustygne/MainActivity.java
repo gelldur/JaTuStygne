51dbffe7a71ebea4b1d57152deb166d9fe452080
Dawid Drozd <drozddawid.uam@gmail.com>
2015-11-18 19:37:58 +0100

    Sample implementation of handling events

--- app/src/main/java/pl/jatustygne/MainActivity.java
+++ app/src/main/java/pl/jatustygne/MainActivity.java
@@ -3,11 +3,45 @@ package pl.jatustygne;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import pl.jatustygne.bus.EventCofeeTemperature;
+import pl.jatustygne.bus.EventCoffeIsCold;
+import pl.jatustygne.bus.EventCoffeIsGood;
+import pl.jatustygne.bus.EventCoffeToHot;
+import pl.jatustygne.bus.GlobalEventBus;
+
 public class MainActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		GlobalEventBus.getInstance().register(this);
+	}
+
+	@Override
+	protected void onPause() {
+		GlobalEventBus.getInstance().unregister(this);
+		super.onPause();
+	}
+
+	public void onEvent(EventCofeeTemperature event) {
+
+	}
+
+	public void onEvent(EventCoffeIsCold event) {
+	
+	}
+
+	public void onEvent(EventCoffeIsGood event) {
+
+	}
+
+	public void onEvent(EventCoffeToHot event) {
+
 	}
 }
